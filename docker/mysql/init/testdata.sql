3b294729ce8142edc03c44abfe52ad6654e33eea
thimi0412 <thimi0412@icloud.com>
2019-03-12 17:56:11 +0900

    update Todoに期限日、登録日、更新日を追加

--- docker/mysql/init/testdata.sql
+++ docker/mysql/init/testdata.sql
@@ -1,5 +1,5 @@
 INSERT INTO users (id,email, password) VALUES (1,'kosuke-shimizu@tech-blue.com', 'ssssssss');
 INSERT INTO users (id,email, password) VALUES (2,'shunpe-saito@tech-blue.com', 'kokokokokoko');
-INSERT INTO todos (id, user_id,context) VALUES(1,1,'Goやる');
-INSERT INTO todos (id, user_id,context) VALUES(2,1,'scikit-learnやる');
-INSERT INTO todos (id, user_id,context) VALUES(3,2,'pythonやる');
+INSERT INTO todos (id,user_id,context,limit_date,insert_date,updated_date) VALUES(1,1,'Goやる','2019-03-20 00:00:00', '2019-03-12 00:00:00', '2019-03-12 00:00:00');
+INSERT INTO todos (id,user_id,context,limit_date,insert_date,updated_date) VALUES(2,1,'scikit-learnやる','2019-03-20 11:30:00', '2019-03-12 00:00:00', '2019-03-12 00:00:00');
+INSERT INTO todos (id,user_id,context,limit_date,insert_date,updated_date) VALUES(3,2,'pythonやる','2019-03-19 12:00:00', '2019-03-12 00:00:00', '2019-03-12 00:00:00');
