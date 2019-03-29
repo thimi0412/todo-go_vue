c84be66ddf57ac2fd78bb78a953cdf40d1dea294
thimi0412 <thimi0412@icloud.com>
2019-03-29 18:19:33 +0900

    update jwtで承認

--- docker/golang/hash.go
+++ docker/golang/hash.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"log"
+
+	jwt "github.com/dgrijalva/jwt-go"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -14,4 +17,38 @@ func passwordHash(pw string) (string, error) {
 
 func passwordVerify(hash, pw string) error {
 	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(pw))
+}
+
+// JWTToken : JWT用のstruct
+type JWTToken struct {
+	UserID    int    `json:"user_id"`
+	UserEmail string `json:"email"`
+	jwt.StandardClaims
+}
+
+func createTokenString(user User) string {
+	token := jwt.NewWithClaims(jwt.GetSigningMethod("HS256"), &JWTToken{
+		UserID:    user.ID,
+		UserEmail: user.Email,
+	})
+
+	tokenstring, err := token.SignedString([]byte("foobar"))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return tokenstring
+}
+
+func authTokenString(tokenstring string) (int, error) {
+
+	token, err := jwt.Parse(tokenstring, func(token *jwt.Token) (interface{}, error) {
+		return []byte("foobar"), nil
+	})
+
+	if err == nil {
+		claims := token.Claims.(jwt.MapClaims)
+
+		return int(claims["user_id"].(float64)), err
+	}
+	return 0, err
 }
