a58f7f853fd3576afe960b87478c1fbedb21b08c
thimi0412 <thimi0412@icloud.com>
2019-03-29 15:39:39 +0900

    update カラム名を変更

--- docker/mysql/init/1.todo.sql
+++ docker/mysql/init/1.todo.sql
@@ -3,7 +3,7 @@ CREATE TABLE todos (
     user_id INT NOT NULL,
     context VARCHAR(255) NOT NULL,
     limit_date DATETIME NOT NULL,
-    insert_date DATETIME NOT NULL,
-    updated_date DATETIME NOT NULL,
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME NOT NULL,
     PRIMARY KEY (id)
 );
--- docker/mysql/init/testdata.sql
+++ docker/mysql/init/testdata.sql
@@ -1,5 +1,5 @@
 INSERT INTO users (id,email, password) VALUES (1,'kosuke-shimizu@tech-blue.com', 'ssssssss');
 INSERT INTO users (id,email, password) VALUES (2,'shunpe-saito@tech-blue.com', 'kokokokokoko');
-INSERT INTO todos (id,user_id,context,limit_date,insert_date,updated_date) VALUES(1,1,'Goやる','2019-03-20 00:00:00', '2019-03-12 00:00:00', '2019-03-12 00:00:00');
-INSERT INTO todos (id,user_id,context,limit_date,insert_date,updated_date) VALUES(2,1,'scikit-learnやる','2019-03-20 11:30:00', '2019-03-12 00:00:00', '2019-03-12 00:00:00');
-INSERT INTO todos (id,user_id,context,limit_date,insert_date,updated_date) VALUES(3,2,'pythonやる','2019-03-19 12:00:00', '2019-03-12 00:00:00', '2019-03-12 00:00:00');
+INSERT INTO todos (id,user_id,context,limit_date,created_at,updated_at) VALUES(1,1,'Goやる','2019-03-20 00:00:00', '2019-03-12 00:00:00', '2019-03-12 00:00:00');
+INSERT INTO todos (id,user_id,context,limit_date,created_at,updated_at) VALUES(2,1,'scikit-learnやる','2019-03-20 11:30:00', '2019-03-12 00:00:00', '2019-03-12 00:00:00');
+INSERT INTO todos (id,user_id,context,limit_date,created_at,updated_at) VALUES(3,2,'pythonやる','2019-03-19 12:00:00', '2019-03-12 00:00:00', '2019-03-12 00:00:00');
